bf66cb08a9337697ae6ba08a94f54697eb57920d
ahmed nasser <ahmednas197@gmail.com>
2023-08-26 21:45:40 +0300

    added dark mode

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,13 +1,14 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <>
-    <UserButton afterSignOutUrl="/"/>
+      <UserButton afterSignOutUrl="/" />
       <Button >Add Server</Button>
       <p className='text-3xl font-bold text-indigo-500'>Hello discord clone</p>
-      
+<ModeToggle />
     </>
   )
 }
