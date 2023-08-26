8293bcf50f04071f376f769af33d64c403323d26
ahmed nasser <ahmednas197@gmail.com>
2023-08-26 18:01:32 +0300

    Folder setup

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <Button >Add Server</Button>
+      <p className='text-3xl font-bold text-indigo-500'>Hello discord clone</p>
+    </>
+  )
+}
