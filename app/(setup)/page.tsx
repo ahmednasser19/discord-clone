d3643f4622dc5e55f90e3f1c385fcdf271516b36
ahmed nasser <ahmednas197@gmail.com>
2023-08-31 14:31:27 +0300

    server creation API

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,9 +1,10 @@
-import InitialModal from "@/components/modals/initial-modal";
+import { redirect } from "next/navigation";
+
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
-import { redirect } from "next/navigation";
-const SetupPage = async () => {
+import { InitialModal } from "@/components/modals/initial-modal";
 
+const SetupPage = async () => {
     const profile = await initialProfile();
 
     const server = await db.server.findFirst({
@@ -17,10 +18,10 @@ const SetupPage = async () => {
     });
 
     if (server) {
-        return redirect(`/servers/${server.id}`)
+        return redirect(`/servers/${server.id}`);
     }
 
-    return (<InitialModal />);
+    return <InitialModal />;
 }
 
-export default SetupPage;
+export default SetupPage;  
--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,16 +1,19 @@
-"use client"
+"use client";
 
-import * as z from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
+import axios from "axios";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+
 import {
     Dialog,
-    DialogTitle,
     DialogContent,
+    DialogDescription,
     DialogFooter,
     DialogHeader,
-    DialogDescription,
-} from "@/components/ui/dialog"
+    DialogTitle,
+} from "@/components/ui/dialog";
 import {
     Form,
     FormControl,
@@ -18,30 +21,29 @@ import {
     FormItem,
     FormLabel,
     FormMessage
-} from '@/components/ui/form'
-import { Button } from "@/components/ui/button";
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { useEffect, useState } from "react";
-import FileUpload from "../FileUpload";
+import { Button } from "@/components/ui/button";
+import FileUpload from "../file-upload";
+import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
     name: z.string().min(1, {
-        message: "Server name is required"
+        message: "Server name is required."
     }),
     imageUrl: z.string().min(1, {
-        message: "Server image is required"
-    }),
-})
+        message: "Server image is required."
+    })
+});
 
+export const InitialModal = () => {
+    const [isMounted, setIsMounted] = useState(false);
 
-const InitialModal = () => {
-
-    const [isMounted, setIsMounted] = useState<boolean>(false);
+    const router = useRouter();
 
     useEffect(() => {
         setIsMounted(true);
-
-    }, [])
+    }, []);
 
     const form = useForm({
         resolver: zodResolver(formSchema),
@@ -49,65 +51,89 @@ const InitialModal = () => {
             name: "",
             imageUrl: "",
         }
-    })
+    });
 
     const isLoading = form.formState.isSubmitting;
+
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
-        console.log("values", values)
+        try {
+            await axios.post("/api/servers", values);
+
+            form.reset();
+            router.refresh();
+            window.location.reload();
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    if (!isMounted) return null;
+    if (!isMounted) {
+        return null;
+    }
+
     return (
         <Dialog open>
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
                 <DialogHeader className="pt-8 px-6">
                     <DialogTitle className="text-2xl text-center font-bold">
-                        Customize your profile
+                        Customize your server
                     </DialogTitle>
                     <DialogDescription className="text-center text-zinc-500">
-                        Give your server a personality with name and an image. You can always change it later.
+                        Give your server a personality with a name and an image. You can always change it later.
                     </DialogDescription>
                 </DialogHeader>
-                <Form {...form} >
+                <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                        <div className="space-y-8 px-6 ">
+                        <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                <FormField control={form.control} name="imageUrl" render={({ field }) => (
+                                <FormField
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload
+                                                    endpoint="serverImage"
+                                                    value={field.value}
+                                                    onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
                                     <FormItem>
+                                        <FormLabel
+                                            className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70"
+                                        >
+                                            Server name
+                                        </FormLabel>
                                         <FormControl>
-                                            <FileUpload
-                                                endpoint="serverImage"
-                                                value={field.value}
-                                                onChange={field.onChange}
+                                            <Input
+                                                disabled={isLoading}
+                                                className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                                                placeholder="Enter server name"
+                                                {...field}
                                             />
                                         </FormControl>
+                                        <FormMessage />
                                     </FormItem>
-                                )} />
-                            </div>
-                            <FormField control={form.control} name="name" render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">Server Name</FormLabel>
-                                    <FormControl>
-                                        <Input disabled={isLoading}
-                                            className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
-                                            {...field} placeholder="Enter Server Name" />
-                                    </FormControl>
-                                    <FormMessage>{form.formState.errors.name?.message}</FormMessage>
-                                </FormItem>
-                            )} />
+                                )}
+                            />
                         </div>
                         <DialogFooter className="bg-gray-100 px-6 py-4">
-                            {/* <Button disabled={isLoading} variant="secondary" onClick={() => { }}>Skip</Button> */}
-                            <Button disabled={isLoading} variant={"primary"} type="submit">Create</Button>
+                            <Button variant="primary" disabled={isLoading}>
+                                Create
+                            </Button>
                         </DialogFooter>
-
-
                     </form>
-
                 </Form>
             </DialogContent>
         </Dialog>
-    );
+    )
 }
-
-export default InitialModal;
