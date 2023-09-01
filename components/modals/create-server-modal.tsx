b1111461902ebed20fb55151cf3682cc6acbb217
ahmed nasser <ahmednas197@gmail.com>
2023-09-01 20:22:20 +0300

    Added server modal

--- components/modals/create-server-modal.tsx
+++ components/modals/create-server-modal.tsx
@@ -4,7 +4,6 @@ import axios from "axios";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useEffect, useState } from "react";
 
 import {
     Dialog,
@@ -26,6 +25,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import FileUpload from "../file-upload";
 import { useRouter } from "next/navigation";
+import { useModal } from "@/hooks/use-model-store";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -36,14 +36,12 @@ const formSchema = z.object({
     })
 });
 
-export const InitialModal = () => {
-    const [isMounted, setIsMounted] = useState(false);
-
+export const CreateServerModal = () => {
+    const { isOpen, onClose, type } = useModal()
     const router = useRouter();
 
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
+    const isModalOpen = isOpen && type === "createServer";
+
 
     const form = useForm({
         resolver: zodResolver(formSchema),
@@ -60,22 +58,18 @@ export const InitialModal = () => {
             await axios.post("/api/servers", values);
 
             form.reset();
-
             router.refresh();
-            onclose()
-            window.location.reload();
-
         } catch (error) {
             console.log(error);
         }
     }
 
-    if (!isMounted) {
-        return null;
+    const handleClose = () => {
+        form.reset();
+        onClose()
     }
-
     return (
-        <Dialog open>
+        <Dialog open={isModalOpen} onOpenChange={handleClose}>
             <DialogContent className="bg-white text-black p-0 overflow-hidden">
                 <DialogHeader className="pt-8 px-6">
                     <DialogTitle className="text-2xl text-center font-bold">
