4a420f46d24c649bc30445494343560962a3bd7f
ahmednasser19 <ahmednas197@gmail.com>
2023-10-25 14:51:34 +0300

    server channel list

---
+++ components/server/server-channel.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import { Channel, MemberRole, Server } from "@prisma/client"
+
+interface ServerChannelProps {
+    channel: Channel;
+    server: Server;
+    role?: MemberRole
+}
+export const ServerChannel = (
+    {
+        channel,
+        server,
+        role
+    }: ServerChannelProps
+) => {
+    return (
+        <div>
+            Server Chanell
+        </div>
+    )
+}
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -6,6 +6,9 @@ import ServerHeader from "./server-header";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { ServerSearch } from "./server-search";
 import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
+import { ServerSection } from "./server-section";
+import { ServerChannel } from "./server-channel";
 
 interface ServerSidebarProps {
     serverId: string
@@ -64,10 +67,6 @@ const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
             <ServerHeader server={server} role={role} />
             <ScrollArea className="flex-1 px-3">
                 <div className="mt-2">
-
-                    <div className="">
-
-                    </div>
                     <ServerSearch data={[
                         {
                             label: "Text Channels",
@@ -91,6 +90,15 @@ const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
                         },
                     ]} />
                 </div>
+                <Separator className="bg-zinc-200 dark:bg-slate-700 rounded-md my-2" />
+                {!!textChannels?.length && (
+                    <div className="mb-2">
+                        <ServerSection sectionType="channels" channelType={ChannelType.TEXT} role={role} label="Text Channels" />
+                        {textChannels?.map(channel => (
+                            <ServerChannel key={channel.id} channel={channel} role={role} server={server} />
+                        ))}
+                    </div>
+                )}
             </ScrollArea>
         </div>
     )
